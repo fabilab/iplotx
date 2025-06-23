4de18ba28e4ba49bd784e6c8e6f34b4a599c93fd
Fabio Zanini <fabio.zanini@fastmail.fm>
2025-06-23 22:39:43 +1000

    inplace sed for copying github version

--- scripts/copy_github_release_into_version.sh
+++ scripts/copy_github_release_into_version.sh
@@ -4,4 +4,4 @@ github_release_name=${1}
 # strip v at the beginning
 version=${github_release_name#v}
 
-sed "s/__version__ = \"[0-9\.]\+\"/__version__ = \"${version}\"/" iplotx/version.py
+sed -i "s/__version__ = \"[0-9\.]\+\"/__version__ = \"${version}\"/" iplotx/version.py
