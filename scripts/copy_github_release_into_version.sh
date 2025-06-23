e258d5f5281aff95b209c97e36e168c297c6bec1
Fabio Zanini <fabio.zanini@fastmail.fm>
2025-06-23 22:36:02 +1000

    Try uploading version from github release name before publishing

---
+++ scripts/copy_github_release_into_version.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+github_release_name=${1}
+# strip v at the beginning
+version=${github_release_name#v}
+
+sed "s/__version__ = \"[0-9\.]\+\"/__version__ = \"${version}\"/" iplotx/version.py
