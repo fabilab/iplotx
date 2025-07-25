d08c6e25af4c12068ad149512074f12ff23b996d
Fabio Zanini <fabio.zanini@fastmail.fm>
2025-07-25 22:18:18 +1000

    More arrowheads and improve example

--- gallery/plot_arrows.py
+++ gallery/plot_arrows.py
@@ -9,14 +9,14 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import iplotx as ipx
 
-arrow_markers = ["|>", "|/", "|\\", ">", ">>", ")>", ")", "|", "s", "d", "p", "q"]
+arrow_markers = ["|>", "|/", "|\\", ">", ">>", ")>", ")", "(", "]", "[", "|", "x", "s", "d", "p", "q"]
 n = len(arrow_markers)
 G = nx.DiGraph()
 G.add_edges_from([(f"l{i}", f"r{i}") for i in range(n)])
 layout = {f"l{i}": (0, -i) for i in range(n)}
 layout.update({f"r{i}": (1, -i) for i in range(n)})
 
-fig, ax = plt.subplots(figsize=(3, 7.5))
+fig, ax = plt.subplots(figsize=(3, 8.5))
 ipx.network(
     G,
     layout=layout,
@@ -24,8 +24,10 @@ ipx.network(
     vertex_size=12,
     edge_arrow_marker=arrow_markers,
     edge_arrow_height="width",
+    edge_padding=8,
     edge_color="steelblue",
     title="Arrow markers",
 )
 ax.set_yticks(list(range(-n + 1, 1))[::-1])
 ax.set_yticklabels(arrow_markers)
+ax.yaxis.set_tick_params(length=0)
